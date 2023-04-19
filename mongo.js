7066d170c2128ab32e46dcc4ee816914cabc29e4
Atte Tanskanen <94180117+tan-at@users.noreply.github.com>
2023-04-19 19:34:03 +0300

    3.12 done

---
+++ mongo.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+
+if (process.argv.length < 3) {
+  console.log("give password as argument");
+  process.exit(1);
+}
+
+const password = process.argv[2];
+const personName = process.argv[3];
+const personNumber = process.argv[4];
+
+const url = `mongodb+srv://fullstack:${password}@fullstackcluster.p3bwn5e.mongodb.net/puhelinluetteloApp?retryWrites=true&w=majority`;
+
+mongoose.set("strictQuery", false);
+mongoose.connect(url);
+
+const puhelinluetteloSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+
+const Person = mongoose.model("Person", puhelinluetteloSchema);
+
+if (process.argv.length === 3) {
+  console.log("phonebook:");
+  Person.find({}).then((result) => {
+    result.forEach((person) => {
+      console.log(person.name, person.number);
+    });
+    mongoose.connection.close();
+  });
+} else if (process.argv.length > 3 && process.argv[3]) {
+  const person = new Person({
+    name: personName,
+    number: personNumber,
+  });
+
+  person.save().then((result) => {
+    console.log(`added ${personName} number ${personNumber} to phonebook`);
+    mongoose.connection.close();
+  });
+}
