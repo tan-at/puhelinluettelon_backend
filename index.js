89212ca22da859fbab51180ed5c0fdab031d0a30
Atte Tanskanen <94180117+tan-at@users.noreply.github.com>
2023-04-19 13:21:40 +0300

    3.3 done

--- index.js
+++ index.js
@@ -43,6 +43,17 @@ app.get("/api/persons", (req, res) => {
   res.json(persons);
 });
 
+app.get("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const person = persons.find((person) => person.id === id);
+
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).end();
+  }
+});
+
 const generateId = () => {
   const maxId = persons.length > 0 ? Math.max(...persons.map((n) => n.id)) : 0;
   return maxId + 1;
@@ -57,34 +68,34 @@ app.post("/api/persons", (request, response) => {
     });
   }
 
-  const note = {
+  const person = {
     content: body.content,
     important: body.important || false,
     date: new Date(),
     id: generateId(),
   };
 
-  persons = persons.concat(note);
+  persons = persons.concat(person);
 
-  response.json(note);
+  response.json(person);
 });
 
 app.get("/api/persons/:id", (request, response) => {
   const id = Number(request.params.id);
-  const note = persons.find((note) => note.id === id);
+  const person = persons.find((person) => person.id === id);
 
-  if (note) {
-    response.json(note);
+  if (person) {
+    response.json(person);
   } else {
     response.status(404).end();
   }
 
-  response.json(note);
+  response.json(person);
 });
 
 app.delete("/api/persons/:id", (request, response) => {
   const id = Number(request.params.id);
-  persons = persons.filter((note) => note.id !== id);
+  persons = persons.filter((person) => person.id !== id);
 
   response.status(204).end();
 });
